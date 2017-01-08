509af10a673dfbe85f096e176b10547404072123
changmeng <changmeng@jiadi365.com>
2017-01-08 10:59:20 +0800

    【会员中心】静态页

---
+++ js/SUIMobile/picker.js
@@ -0,0 +1,47 @@
+$(function() {
+  $(document).on("pageInit", function() {
+    $("#picker").picker({
+      toolbarTemplate: '<header class="bar bar-nav">\
+      <button class="button button-link pull-right close-picker">确定</button>\
+      <h1 class="title">请选择你的手机</h1>\
+      </header>',
+      cols: [
+        {
+          textAlign: 'center',
+          values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+        }
+      ]
+    });
+    $("#picker-name").picker({
+      toolbarTemplate: '<header class="bar bar-nav">\
+      <button class="button button-link pull-right close-picker">确定</button>\
+      <h1 class="title">请选择称呼</h1>\
+      </header>',
+      cols: [
+        {
+          textAlign: 'center',
+          values: ['赵', '钱', '孙', '李', '周', '吴', '郑', '王']
+        },
+        {
+          textAlign: 'center',
+          values: ['杰伦', '磊', '明', '小鹏', '燕姿', '菲菲', 'Baby']
+        },
+        {
+          textAlign: 'center',
+          values: ['先生', '小姐']
+        }
+      ]
+    });
+    $("#datetime-picker").datetimePicker({
+      value: ['1985', '12', '04', '9', '34']
+    });
+    $("#city-picker").cityPicker({
+      toolbarTemplate: '<header class="bar bar-nav">\
+      <p class="pull-left close-picker">关闭</p>\
+      <button class="button button-link pull-right close-picker">确定</button>\
+      <h1 class="title"></h1>\
+      </header>'
+    });
+  });
+  $.init();
+});
